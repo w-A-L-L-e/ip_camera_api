f902dca2d4b2ed7d94dd03bfb305fce7f090a6f3
Walter Schreppers <walter@sitweb.eu>
2022-08-24 17:28:56 +0200

    some simple scripts to set some settings and capture a single image easily

---
+++ cam_settings.sh
@@ -0,0 +1,6 @@
+# curl "http://192.168.0.240/control?var=framesize&val=9"  # 800x600
+curl "http://192.168.0.240/control?var=framesize&val=10"  # 1024x768
+
+# set gainceiling to 5 gives brighter image
+curl "http://192.168.0.240/control?var=gainceiling&val=4"
+
---
+++ capture.sh
@@ -0,0 +1,4 @@
+
+wget http://192.168.0.240/capture -O capture1.jpg
+
+
