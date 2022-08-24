3e4e7246921c3abdf60f2d8b03ad91c66429901d
Walter Schreppers <walter@sitweb.eu>
2022-08-24 21:42:31 +0200

    some handy bash camera commands

--- camera_commands/change_settings.sh
+++ camera_commands/change_settings.sh
@@ -4,3 +4,6 @@ curl "http://192.168.0.240/control?var=framesize&val=10"  # 1024x768
 # set gainceiling to 5 gives brighter image
 curl "http://192.168.0.240/control?var=gainceiling&val=4"
 
+# this is not enabled, but should control the led while streaming.
+# curl "http://192.168.0.240/control?var=led_intensity&val=255" 
+# we will however do something different and allow turning on/off led flash with a button
---
+++ camera_commands/reset.sh
@@ -0,0 +1,1 @@
+curl "http://192.168.0.240/control?var=restart&val=1" 
